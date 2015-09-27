99a5b7fbdbea98b6f3a2348a4bab84d9d23b42b2
Derek Anderson <derek@deberon.com>
2015-09-27 13:55:40 -0400

    Reset version number.

--- docs/api_project.js
+++ docs/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-09-27T17:53:14.129Z",
+    "time": "2015-09-27T17:53:51.809Z",
     "url": "http://apidocjs.com",
     "version": "0.13.1"
   }
