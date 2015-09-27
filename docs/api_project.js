a7506d2e736d089a61747dce91e25efbf0c6051a
Travis CI <derek@deberon.com>
2015-09-27 18:25:04 +0000

    [AUTOCOMMIT] Deployed API v0.0.1 docs from commit 0e7aec0.

--- docs/api_project.js
+++ docs/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-09-27T18:24:14.289Z",
+    "time": "2015-09-27T18:25:03.504Z",
     "url": "http://apidocjs.com",
     "version": "0.13.1"
   }
