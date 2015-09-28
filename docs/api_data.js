bdfe1cd354f7b8c93f64041d8b5b00921d13c69d
Travis CI <derek@deberon.com>
2015-09-28 21:41:11 +0000

    [AUTOCOMMIT] Deployed API v0.0.1 docs from commit ea1db17.

--- docs/api_data.js
+++ docs/api_data.js
@@ -52,6 +52,65 @@ define({ "api": [
     "groupTitle": "Stocks"
   },
   {
+    "type": "get",
+    "url": "/v1/stocks",
+    "title": "Fetch a list of stocks",
+    "name": "GetStocks",
+    "description": "<p>Retrieves a list of all stocks</p> ",
+    "group": "Stocks",
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "<p>Array[]</p> ",
+            "optional": false,
+            "field": "stocks",
+            "description": "<p>List of all matched stocks</p> "
+          },
+          {
+            "group": "Success 200",
+            "type": "<p>String</p> ",
+            "optional": false,
+            "field": "stocks.name",
+            "description": "<p>Name of the security</p> "
+          },
+          {
+            "group": "Success 200",
+            "type": "<p>String</p> ",
+            "optional": false,
+            "field": "stocks.symbol",
+            "description": "<p>Stock symbol of the security</p> "
+          },
+          {
+            "group": "Success 200",
+            "type": "<p>String</p> ",
+            "optional": false,
+            "field": "stocks.cik",
+            "description": "<p>SEC CIK number</p> "
+          },
+          {
+            "group": "Success 200",
+            "type": "<p>String</p> ",
+            "optional": false,
+            "field": "stocks.exchange",
+            "description": "<p>The exchange the security is traded on.</p> "
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "{\n    stocks: [\n      {\n          \"name\": \"Apple Inc.\",\n          \"symbol\": \"AAPL\",\n          \"cik\": \"0000320193\",\n          \"exchange\": \"NASDAQ\"\n      }\n   ]\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "src/controllers/stocks/index.js",
+    "groupTitle": "Stocks"
+  },
+  {
     "type": "put",
     "url": "/v1/stocks",
     "title": "Create or modify stock",
--- docs/api_project.js
+++ docs/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-09-27T18:46:40.650Z",
+    "time": "2015-09-28T21:41:11.090Z",
     "url": "http://apidocjs.com",
     "version": "0.13.1"
   }
