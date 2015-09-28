3efb13a3c782a980c12d5d63869f296e3b0db5d4
Travis CI <derek@deberon.com>
2015-09-28 21:58:04 +0000

    [AUTOCOMMIT] Deployed API v0.0.1 docs from commit 0a3638c.

--- docs/api_data.js
+++ docs/api_data.js
@@ -184,37 +184,6 @@ define({ "api": [
   },
   {
     "type": "get",
-    "url": "/v1/version/protected",
-    "title": "Auth-protected API version",
-    "name": "GetProtectedVersion",
-    "description": "<p>Returns the same content as GetVersion, but is auth-protected. This is useful for testing/validating auth mechanisms.</p> ",
-    "group": "Version",
-    "success": {
-      "fields": {
-        "Success 200": [
-          {
-            "group": "Success 200",
-            "type": "<p>String</p> ",
-            "optional": false,
-            "field": "version",
-            "description": "<p>API Version.</p> "
-          }
-        ]
-      },
-      "examples": [
-        {
-          "title": "Success-Response:",
-          "content": "{\n    \"version\": \"1.2.3\"\n}",
-          "type": "json"
-        }
-      ]
-    },
-    "version": "0.0.0",
-    "filename": "src/controllers/version.js",
-    "groupTitle": "Version"
-  },
-  {
-    "type": "get",
     "url": "/v1/version",
     "title": "API build version",
     "name": "GetVersion",
